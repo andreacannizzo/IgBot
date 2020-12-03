bed4fa85ec5fb2267aa95905d11ac37f378425f4
Andrea Cannizzo <andreacannizzo@MacBook-Pro-di-Andrea.local>
2020-12-03 21:19:05 +0100

    added files

---
+++ definitions.py
@@ -0,0 +1,96 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+import time
+import random
+
+
+def lunch_browser(images=True, web_site='https://www.instagram.com/'):
+    if not images:
+        chrome_options = webdriver.ChromeOptions()
+        prefs = {"profile.managed_default_content_settings.images": 2}
+        chrome_options.add_experimental_option("prefs", prefs)
+        browser = webdriver.Chrome('/Users/andreacannizzo/Downloads/chromedriver', chrome_options=chrome_options)
+        browser.get(web_site)
+    else:
+        browser = webdriver.Chrome('/Users/andreacannizzo/Downloads/chromedriver')
+        browser.get(web_site)
+    return browser
+
+
+def cookies_accept(browser):
+    browser.implicitly_wait(10)
+    browser.find_element_by_xpath("//button[text()='Accetta']").click()
+
+
+def login(browser, username_str='lubitelcommunity', password_str='Lubitelpass1992!'):
+    # inserisci username e password nei relativi slot
+    username = browser.find_element_by_name('username')
+    username.clear()
+    username.send_keys(username_str)
+    password = browser.find_element_by_name('password')
+    password.clear()
+    password.send_keys(password_str)
+    # batti invio di conferma
+    submit = browser.find_element_by_tag_name('form')
+    submit.submit()
+
+
+def avoid_popups(browser):
+    # attendi pop up salvataggio credenziali e annullalo
+    WebDriverWait(browser, 15).until(lambda d: d.find_element_by_xpath('//button[text()="Non ora"]')).click()
+    # attendi pop up attivazione notifiche e annullalo
+    browser.implicitly_wait(10)
+    browser.find_element_by_xpath("//button[text()='Non ora']").click()
+
+
+def search_hashtag(browser, hash_str='#lubitel'):
+    # cerca barra ricerca, inserisci hashtag desiderato e batti due volte invio
+    searchbox = WebDriverWait(browser, 10).until(
+        EC.visibility_of_element_located(
+            (By.XPATH, "//input[@placeholder='Cerca']")
+        )
+    )
+    searchbox.send_keys(hash_str)
+    time.sleep(2)
+    searchbox.send_keys(Keys.ENTER)
+    time.sleep(1)
+    searchbox.send_keys(Keys.ENTER)
+
+
+def click_first_pic(browser):
+    # cerca prima immagine della pagina e cliccala
+    first_image = WebDriverWait(browser, 10).until(
+        EC.visibility_of_element_located(
+            (By.XPATH, "//*[@id='react-root']/section/main/article/div[1]/div/div/div[1]/div[1]")
+        )
+    )
+    time.sleep(5)
+    first_image.click()
+
+
+def like_if_its_ok(browser, number):
+    like_xpath = '/html/body/div[5]/div[2]/div/article/div[3]/section[1]/span[1]/button'
+    like = WebDriverWait(browser, 10).until(EC.visibility_of_element_located((By.XPATH, like_xpath)))
+    color = like.get_property("innerHTML")
+    if "#262626" in color:
+        time.sleep(random.uniform(2, 4))
+        like.click()
+        print(number+1)
+        time.sleep(random.uniform(2, 4))
+        return 1
+    else:
+        time.sleep(random.uniform(3, 4))
+        return 0
+
+
+def back_n_forth(browser):
+    time.sleep(10)
+    print("torno indietro")
+    browser.find_element_by_xpath("/html/body/div[5]/div[1]/div/div/a[1]").click()
+    time.sleep(10)
+    print("torno avanti")
+    browser.find_element_by_xpath("/html/body/div[5]/div[1]/div/div/a[2]").click()
+
---
+++ main.py
@@ -0,0 +1,65 @@
+from selenium.common.exceptions import TimeoutException
+from definitions import *
+
+web_site = 'https://www.instagram.com/'
+username_str = 'lubitelcommunity'
+password_str = 'Lubitelpass1992!'
+# username_str = '101izzo'
+# password_str = 'Instagrampass1992'
+# username_str = 'bluinfinitobeb'
+# password_str = 'Instagram1987'
+hash_str = '#lubitel166'
+no_cycle = 100
+
+browser = lunch_browser(False, web_site)
+cookies_accept(browser)
+login(browser, username_str, password_str)
+avoid_popups(browser)
+search_hashtag(browser, hash_str)
+click_first_pic(browser)
+
+# crea variabili che contano il numero di post likati, quelli skipped e il numero di tentativi di refresh max
+no_liked = 0
+no_posts_seen = 0
+max_tentativi = 0
+
+# esegui una volta l'argomento del ciclo for perche' cambia l'xpath del bottone next
+try:
+    no_liked += like_if_its_ok(browser, no_liked)
+    next_post = browser.find_element_by_xpath("/html/body/div[5]/div[1]/div/div/a")
+    next_post.click()
+except TimeoutException:  # Exception:
+    print('non sono riuscito a caricare nemmeno la prima immagine')
+    browser.close()
+    exit()
+no_posts_seen += 1
+
+# esegui il ciclo
+while no_liked < no_cycle:
+    try:
+        no_liked += like_if_its_ok(browser, no_liked)
+        next_post = browser.find_element_by_xpath("/html/body/div[5]/div[1]/div/div/a[2]")
+        next_post.click()
+        no_posts_seen += 1
+    except TimeoutException:
+        if max_tentativi == 3:
+            browser.close()
+            exit()
+        back_n_forth(browser)
+        max_tentativi += 1
+
+# esegui l'ultimo visualizzato
+no_liked += like_if_its_ok(browser, no_liked)
+no_posts_seen += 1
+
+# display stat
+print('desiderati minimo:')
+print(no_cycle)
+print('ottenuti like: (ricorda che al massimo ne puo mettere due i piu dei desiderati)')
+print(no_liked)
+print('post osservati:')
+print(no_posts_seen)
+
+# close browser and exit
+browser.close()
+
