55de942d822d7fb77ed74e1ee379d1f81fff256f
andreacannizzo <cannizzo1@hotmail.com>
2020-12-04 20:45:05 +0100

    delete inputs.py file

--- inputs.py
+++
@@ -1,6 +0,0 @@
-username_str = '<your_username>'
-password_str = '<your_password>'
-hash_str = '#<hashtag>'
-#number of likes you want IgBot to do before exiting
-no_cycle = 100
-path_to_chromedriver = '<your/path/to/chromedriver>'
