222170cae5123bfa3a2301223a40fbc486b44313
andreacannizzo <cannizzo1@hotmail.com>
2020-12-05 01:47:37 +0100

    multiple hashtags research

--- main.py
+++ main.py
@@ -13,38 +13,39 @@ browser = lunch_browser(path_to_chromedriver, False)
 cookies_accept(browser)
 login(browser, username_str, password_str)
 avoid_popups(browser)
-search_hashtag(browser, hash_str)
-time.sleep(5)
-scroll_down(browser)
-click_first_pic(browser)
 
+for hash_i in hash_str:
+    print(f"searching for {hash_i} posts")
+    search_hashtag(browser, hash_i)
+    time.sleep(5)
+    scroll_down(browser)
+    click_first_pic(browser)
 
-# create variables that counts liked posts, total viewed, tryings and max number of tryings
-liked = 0
-total = 0
-tryings = 0
-max_tryings = 3
+    # create variables that counts liked posts, total viewed, tryings and max number of tryings
+    liked = 0
+    total = 0
+    tryings = 0
+    max_tryings = 3
 
+    # starting from the most recent post there is no need to change the xpath of the 'next_post' because they are all equal
+    while liked < target_of_likes:
+        try:
+            liked += like_if_its_ok(browser, liked)
+            browser.find_element_by_xpath("/html/body/div[5]/div[1]/div/div/a[2]").click()
+            total += 1
+        except TimeoutException:
+            if tryings == max_tryings:
+                print(f'image did not load after {max_tryings} attempts')
+                browser.close()
+                exit()
+            back_n_forth(browser)
+            tryings += 1
 
-# starting from the most recent post there is no need to change the xpath of the 'next_post' because they are all equal
-while liked < target_of_likes:
-    try:
-        liked += like_if_its_ok(browser, liked)
-        browser.find_element_by_xpath("/html/body/div[5]/div[1]/div/div/a[2]").click()
-        total += 1
-    except TimeoutException:
-        if tryings == max_tryings:
-            print(f'image did not load after {max_tryings} attempts')
-            browser.close()
-            exit()
-        back_n_forth(browser)
-        tryings += 1
+    # display outcome
+    print(f'target of likes = {target_of_likes}')
+    print(f'likes = {liked}')
+    print(f'total viewed = {total}')
 
-
-# display outcome
-print(f'target of likes = {target_of_likes}')
-print(f'likes = {liked}')
-print(f'total viewed = {total}')
 
 # close browser and exit
 browser.close()
