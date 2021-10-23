9d2b705f9da560700af268f8637650df6c7b38f6
occhiodelloshinigami <cannizzo1@hotmail.com>
2021-10-23 13:55:43 +0200

    changed likes timing

--- definitions.py
+++ definitions.py
@@ -114,12 +114,12 @@ def like_if_its_ok(browser, number, like_xpath):
         EC.visibility_of_element_located((By.XPATH, like_xpath)))
     color = like.get_property("innerHTML")
     if "#262626" in color:
-        time.sleep(random.uniform(3, 5))
+        time.sleep(random.uniform(2, 4))
         like.click()
-        time.sleep(random.uniform(3, 5))
+        time.sleep(random.uniform(2, 4))
         return 1
     else:
-        time.sleep(random.uniform(3, 5))
+        time.sleep(random.uniform(2, 4))
         return 0
 
 
